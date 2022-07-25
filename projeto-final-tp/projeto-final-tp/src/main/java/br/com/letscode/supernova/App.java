9ae9db71cbe0d59c2c68d00014ff17b5b7c44f26
F4463466 Italo Franchesco Magalhaes Feitosa <italo.feitosa@bb.com.br>
2022-07-25 18:09:23 -0300

    Mudança para uso do ExecutorService para execução em Threads.

--- projeto-final-tp/projeto-final-tp/src/main/java/br/com/letscode/supernova/App.java
+++ projeto-final-tp/projeto-final-tp/src/main/java/br/com/letscode/supernova/App.java
@@ -5,7 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import br.com.letscode.supernova.csv.RepositorioFilmes;
@@ -20,34 +24,38 @@ public class App {
 
 		LocalDateTime inicio = LocalDateTime.now();
 		long inicioMs = System.currentTimeMillis();
+		ExecutorService executor = Executors.newCachedThreadPool();
 		final RepositorioFilmes repositorio = new RepositorioFilmes(filmes);
-	
+
 		repositorio.stream().map(filme -> filme.getYear()).distinct()
-				.forEach(ano -> new Thread(new RankeadorPorAno(ano, repositorio)).start());
+			.forEach(ano -> executor.submit(new RankeadorPorAno(ano, repositorio)));
 		
 		Runnable horror = () -> {
 			new SumidouroDoHorror("horror-melhores.txt").processar(repositorio.stream());
 		};
-		Thread thorror = new Thread(horror);
-		thorror.start();
-
-		while (Thread.activeCount() > 1) { }
+		executor.submit(horror);
+		
+		executor.shutdown();
+		
+		try {
+			executor.awaitTermination(10, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			System.err.println("Threads interrompidos.");
+		}
 
 		long duracao = System.currentTimeMillis() - inicioMs;
 		LocalDateTime termino = LocalDateTime.now();
 		System.out.println("Hora de início: " + inicio);
 		System.out.println("Hora de término: " + termino);
 		System.out.println("Tempo de execução: " + duracao + " ms.");
-		
-		
+
 		long tempo = 0;
-		List<String> temporizacaoDeExecucao = List.of(
-			String.format("Início de processamento: %s", inicio),
-			String.format("Final de processamento: %s", LocalDateTime.now()),
-			String.format("Tempo em milissegundos: %d ms", tempo = System.currentTimeMillis() - inicioMs),
-			String.format("Tempo de execução em segundos: %.2f s", (double) tempo / 1000)
-		);
-		Files.write(Path.of("tempo-de-execucao.txt"), temporizacaoDeExecucao, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+		List<String> temporizacaoDeExecucao = List.of(String.format("Início de processamento: %s", inicio),
+				String.format("Final de processamento: %s", LocalDateTime.now()),
+				String.format("Tempo em milissegundos: %d ms", tempo = System.currentTimeMillis() - inicioMs),
+				String.format("Tempo de execução em segundos: %.2f s", (double) tempo / 1000));
+		Files.write(Path.of("tempo-de-execucao.txt"), temporizacaoDeExecucao, StandardOpenOption.TRUNCATE_EXISTING,
+				StandardOpenOption.CREATE);
 	}
 }
 
